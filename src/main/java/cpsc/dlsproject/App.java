75442ada4a1369e4f7bc567fdcf70e74d54f49ad
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-09-22 16:53:48 -0700

    Starter code

---
+++ src/main/java/cpsc/dlsproject/App.java
@@ -0,0 +1,13 @@
+package cpsc.dlsproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
