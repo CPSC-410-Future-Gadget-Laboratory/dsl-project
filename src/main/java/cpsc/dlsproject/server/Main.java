edbd017cd7eaa827f421333bf98fb9bc3af44924
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-10-01 00:26:36 -0700

    Wrote server creation code, and added Java-8 lambda support

---
+++ src/main/java/cpsc/dlsproject/server/Main.java
@@ -0,0 +1,30 @@
+package cpsc.dlsproject.server;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public final class Main {
+  /** Testing out the Server Class */
+  public static void main(String[] args) throws IOException {
+    Server server = Server.newBuilder().setPort(8000).build();
+    server
+        .createEndpoint("/")
+        .setHandler(
+            "/",
+            httpExchange -> {
+              byte response[] = "Hello, World!!!".getBytes("UTF-8");
+
+              httpExchange.getResponseHeaders().add("Content-Type", "text/html; charset=UTF-8");
+              httpExchange.sendResponseHeaders(200, response.length);
+
+              OutputStream out = httpExchange.getResponseBody();
+              out.write(response);
+              out.close();
+            })
+        .startServer();
+  }
+}
