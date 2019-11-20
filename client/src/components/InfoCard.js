1dbb501b1c700ed665cb80010564fe1d2f372a6b
Michael Englo <menglo1@students.cs.ubc.ca>
2019-11-20 15:54:29 -0800

    setup work for everybody

---
+++ client/src/components/InfoCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+export default class InfoCard extends React.Component {
+    render() {
+        return (
+            <Card>
+                {/* Things to do:
+                - Check out React Documentation https://reactjs.org/docs/components-and-props.html
+                - For this component, feel free to propose an appropriate API via props that you need to display the info.
+                - Create a dummy data for development purpose for now first.
+                - Propose what information would be useful to show for each node (request, endpoint, etc).
+                - Read up SemantiUI React library and browse the components that can help you implement it. (https://react.semantic-ui.com/) */}
+                Implement Info Card here... (Change this text and see it reloads automagically.)
+            </Card>
+        )
+    }
+}
