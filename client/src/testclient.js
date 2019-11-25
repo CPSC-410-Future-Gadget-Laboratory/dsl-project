e366a9085fb4a7758a118601605541a373b57481
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-11-25 02:11:17 -0800

    Making requests to multiple endpoints at random

--- client/src/testclient.js
+++ client/src/testclient.js
@@ -1,25 +1,60 @@
 const http = require("http");
 
-let cmdArgs = process.argv.slice(2);
+const options = [
+    {
+        hostname: "localhost",
+        port: 8000,
+        path: "/path/to/success",
+        method: "GET"
+    },
+    {
+        hostname: "localhost",
+        port: 8000,
+        path: "/",
+        method: "GET"
+    },
+    {
+        hostname: "localhost",
+        port: 8000,
+        path: "/path",
+        method: "GET"
+    },
+    {
+        hostname: "localhost",
+        port: 8000,
+        path: "/to",
+        method: "GET"
+    },
+    {
+        hostname: "localhost",
+        port: 8000,
+        path: "/index",
+        method: "GET"
+    },
+    {
+        hostname: "localhost",
+        port: 8000,
+        path: "/success",
+        method: "GET"
+    }
+];
 
-const options = {
-    hostname: "localhost",
-    port: 8000,
-    path: cmdArgs[0],
-    method: "GET"
-};
-
-for (let i = 0; i < cmdArgs[1]; i++) {
-    const req = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        res.on('data', d => {
-            process.stdout.write(d)
+for (let i = 0; i < options.length; i++) {
+    let numRequests = getRandomInt(50);
+    for (let j = 0; j < numRequests; j++) {
+        const req = http.request(options[i], res => {
+            console.log(`statusCode: ${res.statusCode}`)
+            res.on('data', d => {
+                process.stdout.write(d)
+            })
+        });
+        req.on('error', error => {
+            console.error(error)
         })
-    });
+        req.end();
+    }
+}
 
-    req.on('error', error => {
-        console.error(error)
-    })
-    
-    req.end();
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
